d83ea78448e789003a4cfa9eaf26d480fcb2984b
choochoo2610-cmyk <choochoo2610@gmail.com>
2026-01-12 17:59:19 +0900

    Implement data export functionality
    Add exportData function to download JSON data

--- script.js
+++ script.js
@@ -177,3 +177,15 @@ totalMin += workMin;
 window.onload = () => {
   render();
 };
+// ===== 閲覧用データ出力 =====
+function exportData() {
+  const blob = new Blob(
+    [JSON.stringify(data, null, 2)],
+    { type: "application/json" }
+  );
+
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(blob);
+  a.download = "timecard.json";
+  a.click();
+}
